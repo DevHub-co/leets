0754e1c97cb698243ec9f511edf4570d7bd879a5
devHub24 <devhub2024@gmail.com>
2025-08-20 14:20:43 +0530

    Longest Common Prefix

--- ArraysAndStrings/src/com/sk/strings/longestPrefix/Main.java
+++ ArraysAndStrings/src/com/sk/strings/longestPrefix/Main.java
@@ -6,6 +6,20 @@ public class Main {
         trie.insert("dog");
         trie.insert("racecar");
         trie.insert("flight");
-        System.out.println(trie.getPrefix());
+       // System.out.println(trie.getPrefix());
+        String[] strArr = new String[]{"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strArr));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
     }
 }
